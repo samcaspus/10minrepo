ed81e70974e0ec1d36df070340efae1c05e27dcf
samcaspus <sandeepguptan1998@gmail.com>
2021-05-14 21:35:10 +0530

    time constraints

--- youtubeViewer.js
+++ youtubeViewer.js
@@ -1,25 +1,38 @@
 
 const {chromium} = require('playwright');
-let time = 1;
 
 
 (
+
     async () =>{
+        let links = ['https://youtu.be/178hMKR4_-0','https://youtu.be/178hMKR4_-0']
+
+
+        for(let link of links){
+            console.log(link)
+        
         const browser = await chromium.launch({headless: false});
         const page = await browser.newPage()
 
-        await page.goto('https://youtu.be/178hMKR4_-0')
-        await page.evaluate(()=>{
+        await page.goto(link)
+        let duration = await page.evaluate(()=>{
+
+            let semiDuration = document.querySelector('[class="ytp-time-duration"]').innerHTML;
             try{
             document.querySelector('[aria-label="Play (k)"]').click()
             }catch(e){
                 console.log(e);
             }
-        })
+
+            return semiDuration
+        });
+
+        console.log(`waiting for ${parseInt(duration.split(":")[0])} min`);
         
-        setTimeout(()=>{
-            browser.close()
+        await page.waitForTimeout(parseInt(duration.split(":")[0])*60000)
+        await browser.close()
             
-        },(time*60000))
+       
     }
+}
 )();
